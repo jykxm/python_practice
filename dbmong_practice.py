67a58fe4150fc85a05fd62ac8cdebcddddba3469
jykxm <129764645+jykxm@users.noreply.github.com>
2023-04-19 04:57:48 +0900

    Update dbmong_practice.py

--- dbmong_practice.py
+++ dbmong_practice.py
@@ -1,1 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
 
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://sparta:test@cluster0.uhhdzgs.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+URL = "https://movie.daum.net/ranking/reservation"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(URL, headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+video_list = soup.select("#mainContent > div > div.box_ranking > ol > li")
+for v in video_list:
+    rank = v.select_one(".rank_num").text
+    title = v.select_one(".tit_item").text.strip("\n")
+    rate = v.select_one(".txt_grade").text
+    doc = {'title': title, 'rank': rank, 'rate': rate}
+    db.movies.insert_one(doc)
